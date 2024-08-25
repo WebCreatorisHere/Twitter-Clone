eb618d93cc974019cd668807c886871aae65b9b3
WebCreatorisHere <pratibhad.amit@gmail.com>
2024-08-26 01:29:07 +0530

    Added Projectgit add .!

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors:{
+        "btncolor":"#1d9bf0",
+        "btncolorwhite":"#f0f4f5",
+        "lihover":"#171717",
+        "search":"#1f2226",
+        "rehnde":"#6699ff",
+        "most":"#1d9bf0",
+        
+      },
+    },
+  },
+  plugins: [],
+}
+
